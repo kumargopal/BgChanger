cdd7002529b107b6c3604923d943f06241e22e16
Gopal Kumar <email.krgopal@gmail.com>
2024-06-22 14:59:30 +0530

    basic FrontEnd Fixed

--- FrontEnd/static/js/script.js
+++ FrontEnd/static/js/script.js
@@ -1,26 +1,28 @@
-document.getElementById('inpainting-form').addEventListener('submit', async function(event) {
-    event.preventDefault();
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('inpainting-form').addEventListener('submit', async function(event) {
+        event.preventDefault();
 
-    const formData = new FormData(this);
+        const formData = new FormData(this);
 
-    const response = await fetch('/process', {
-        method: 'POST',
-        body: formData,
-    });
-
-    if (response.ok) {
-        const resultImages = await response.json();
-        const resultContainer = document.getElementById('result-container');
-        resultContainer.innerHTML = '';
-
-        resultImages.forEach(imgBase64 => {
-            const imgElement = document.createElement('img');
-            imgElement.src = imgBase64;
-            imgElement.style.width = '300px';  // Adjust size as needed
-            imgElement.style.margin = '10px';
-            resultContainer.appendChild(imgElement);
+        const response = await fetch('/process', {
+            method: 'POST',
+            body: formData,
         });
-    } else {
-        alert('Error generating images');
-    }
+
+        if (response.ok) {
+            const resultImages = await response.json();
+            const resultContainer = document.getElementById('result-container');
+            resultContainer.innerHTML = '';
+
+            resultImages.forEach(imgBase64 => {
+                const imgElement = document.createElement('img');
+                imgElement.src = imgBase64;
+                imgElement.style.width = '300px';  // Adjust size as needed
+                imgElement.style.margin = '10px';
+                resultContainer.appendChild(imgElement);
+            });
+        } else {
+            alert('Error generating images');
+        }
+    });
 });
